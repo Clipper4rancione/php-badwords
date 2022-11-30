8e36304a1ad951da24c92d76aec5debf55419de9
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-11-30 17:24:24 +0100

    push completed exercise

--- page.php
+++ page.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <?php 
 
-    $frase = $_GET('frase');
-    $badword = $_GET('badword');
+    $frase = $_GET['frase'];
+    $badword = $_GET['badword'];
 
 ?>
 
@@ -14,12 +14,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
 
     <title>PHP Badwords</title>
 </head>
 <body>
-        <h1>
-            <?php echo $frase; ?>
-        </h1>
+      <div class="container p-5">
+        <ul>
+            <li> 
+                <h3>La tua frase: <span> <?php echo $frase ?></span></h3>
+                <ul>
+                    <li>
+                        Numero Caratteri: <?php echo strlen($frase) ?> 
+                    </li>
+                </ul>
+            </li>
+            <li><h3>La parola che hai scelto di censurare: <?php echo $badword ?></h3></li>
+            <li> 
+                <h3>La tua frase censurata:
+                        <?php
+                            $censored_text = str_replace($badword, '***',$frase); 
+                            echo $censored_text;
+                        ?>
+                </h3>
+                <ul>
+                    <li>Numero caratteri <?php echo strlen($censored_text) ?> </li>
+                </ul>
+            </li>
+            
+        </ul>
+      </div>
 </body>
 </html>
